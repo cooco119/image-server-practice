6c576e029908c93d39d371756859d7aa540036d5
JaeJun.Lee <jjlee@deepbio.co.kr>
2018-12-24 17:43:16 +0900

    Implement imageviewer app views.py & Test to fix bugs

--- imageviewer/views.py
+++ imageviewer/views.py
@@ -1,25 +1,28 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from models.models import Users
+from models.models import Image
+from models.serializers import UsersSerializer
+from rest_framework.views import APIView
+from rest_framework.renderers import JSONRenderer
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.serializers import Serializer
+import json
 
-def index(request):
-    return HttpResponse("Hello from image viewer index")
 
-def fetch_image():
-    '''
-    get image from db server
-    '''
+class GetImageHandler(APIView):
+    renderer_classes = (JSONRenderer, )
+    
+    def get(self,request, format=None):
+        return Response(status=status.HTTP_200_OK)
 
-def update_panel():
-    '''
-    update the image viewer panel
-    '''
-
-def draw_panel():
-    '''
-    draw a panel for showing images
-    '''
-
-def click_handler():
-    '''
-    when clicks, magnify image in new, upper layer
-    '''
+    def post(self, request, format=None):
+        reqData = request.data
+        name = reqData.get("name")
+        images = Image.objects.filter(is_private=False).values() | Image.objects.filter(is_private=True).filter(user__name=name).values()
+        image_list = [image for image in images]
+        m_status = status.HTTP_200_OK
+        data = {
+            "status": m_status,
+            "image_list": image_list
+        }
+        return Response(data=json.dumps(data), status=m_status, content_type='application/json')
