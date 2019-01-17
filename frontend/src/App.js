f16ae3ac1e242293dc349287d7ac0ebb7bf2a9e7
JaeJun.Lee <cooco119@gmail.com>
2019-01-17 17:39:15 +0900

    Change file reading to use blobs

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -356,8 +356,6 @@ class App extends Component {
       }
       let f = files[i];
       console.log(filestring);
-      let filestream = fileReaderStrem(f);
-      console.log(filestream);
       let bucketName = this.state.bucketName;
       let imageName = f.name;
       let imageServerInfo = await this.getImageServerInfo(bucketName, imageName);
@@ -371,7 +369,7 @@ class App extends Component {
       let offset = 0;
       this.setState({uploadProgress: 0, readingProgress: 0});
 
-      reader.onloadend = async (e) => {
+      reader.onload = async (e) => {
         chunks.push(e.target.result);
         if (offset < f.size){
           offset += e.target.result.length;
@@ -397,7 +395,7 @@ class App extends Component {
         }
       }
       let blob = f.slice(offset, offset + CHUNK_SIZE);
-      reader.readAsDataURL(f);
+      reader.readAsDataURL(blob);
       
       // Upload to Minio server
       
