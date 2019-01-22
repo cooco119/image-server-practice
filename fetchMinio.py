6dc9e9f23ce608cc68ab1ebca56f578509bf50bc
JaeJun.Lee <cooco119@gmail.com>
2019-01-22 13:22:42 +0900

    Remove thread when it should be

--- fetchMinio.py
+++ fetchMinio.py
@@ -73,13 +73,14 @@ def setQueueAndStart():
                 print("Successfully pulled " + finishedBucketName)
                 numThreads -= 1
 
-        for idx in threadIdxToBePoped.sort(reverse=True):
-            try:
-                threadList.pop(idx)
-            except Exception as e:
-                logger.error(e)
-                threadIdxToBePoped.remove(idx)
-        threadIdxToBePoped = []
+        if threadIdxToBePoped:
+            for idx in threadIdxToBePoped.sort(reverse=True):
+                try:
+                    threadList.pop(idx)
+                except Exception as e:
+                    logger.error(e)
+                    threadIdxToBePoped.remove(idx)
+            threadIdxToBePoped = []
 
         if len(threadList) == 0:
             return
